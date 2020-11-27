723e2753633f5b65d3354ed190f99ebcacf8f6f1
serhii-honchar <sergyi.gonchar@gmail.com>
2020-11-27 23:16:50 +0200

    impl: fixed path

--- kafka/consumerApp/runKafkaConsumer.sh
+++ kafka/consumerApp/runKafkaConsumer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-java -jar ./target/kafkaConsumer-0.0.1-spring-boot.jar \
+java -jar ./big-data-tools/kafka/consumerApp/target/kafkaConsumer-0.0.1-spring-boot.jar \
  --cron="*/10 * * * * *" \
  --readRate=5000 \
  --spring.kafka.bootstrap-servers=localhost:9092 \
