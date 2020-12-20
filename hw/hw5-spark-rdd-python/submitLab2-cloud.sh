3cfb232cbed1cda40f1747cb6ce02a5f6962f619
serhii-honchar <sergyi.gonchar@gmail.com>
2020-12-21 00:42:10 +0200

    fixed scripts for running jobs in cloud

--- hw/hw5-spark-rdd-python/submitLab2-cloud.sh
+++ hw/hw5-spark-rdd-python/submitLab2-cloud.sh
@@ -1,9 +1,10 @@
-./spark-submit --master yarn-cluster  \
-  --num-executors 20 --executor-memory 1G --executor-cores 3 \
+spark-submit \
+ --master yarn --deploy-mode cluster \
+ --num-executors 20 --executor-memory 1G --executor-cores 1 \
  --driver-memory 1G \
  --conf spark.yarn.appMasterEnv.SPARK_HOME=/dev/null \
  --conf spark.executorEnv.SPARK_HOME=/dev/null \
- --files  ../../spark/rdd/python/lab2.py \
+ ../../spark/rdd/python/lab2.py \
  hdfs:///bdpc/hadoop_mr/flight-delays/flights.csv \
  hdfs:///bdpc/hadoop_mr/flight-delays/airports.csv \
  hdfs:///bdpc/hadoop_mr/flight-delays/airlines.csv hdfs:///bdpc/spark/output/
